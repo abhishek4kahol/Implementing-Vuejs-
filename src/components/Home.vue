8da2f04b1f2478f8566a29b68db44bae8bde4f7a
abhishek_kahol <intern.abhishek.kahol@firstcry.com>
2017-07-20 18:44:35 +0530

    creating repo

---
+++ src/components/Home.vue
@@ -0,0 +1,60 @@
+<template>
+
+</template>
+<script>
+// import stores from '@/stores'
+
+export default {
+  name: 'Home',
+  // created () {
+  //   this.refreshInfo()
+  // },
+  data () {
+    return {
+      profileMenu: false,
+      actionMenu: false,
+      notificationMenu: false,
+      sidemenutoggle: false,
+      menus: [ {
+        name: 'Home',
+        icon: 'fa fa-home',
+        location: '/dashboard'
+      } ],
+      loggedInUser: null
+    }
+  },
+  methods: {
+    refreshInfo: function () {
+      // stores.user.refreshInfo(true)
+      //   .then((userInfo) => {
+      //     this.loggedInUser = userInfo
+        // })
+    },
+    toggleSidebar: function () {
+      this.sidemenutoggle = !this.sidemenutoggle
+    },
+    toggleNotif: function () {
+      this.collapseAllHeaderDropDowns()
+      this.notificationMenu = true
+    },
+    toggleAction: function () {
+      this.collapseAllHeaderDropDowns()
+      this.actionMenu = true
+    },
+    toggleProfile: function () {
+      this.collapseAllHeaderDropDowns()
+      this.profileMenu = true
+    },
+    collapseAllHeaderDropDowns: function () {
+      this.profileMenu = false
+      this.actionMenu = false
+      this.notificationMenu = false
+    },
+    logout: function () {
+      console.warn('Logging out')
+      // window.localStorage.clear()
+      // this.$router.push({name: 'Login'})
+    }
+  }
+}
+</script>
